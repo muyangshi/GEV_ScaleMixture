19d0415acb7e4370c274295f6525d3e37afb6807
Muyang <34021089+muyangshi@users.noreply.github.com>
2023-09-10 18:25:50 -0600

    Create test_mpi.py

---
+++ test_mpi.py
@@ -0,0 +1,17 @@
+#%%
+# Imports and Set Parameters
+import os
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=1
+
+import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+print('my rank is: ', rank)
