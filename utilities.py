4b545fd18f7f72e8560157985f256ce064e0a248
Muyang <34021089+muyangshi@users.noreply.github.com>
2023-11-07 14:03:36 -0700

    organize 4 integration methods

--- utilities.py
+++ utilities.py
@@ -1,24 +1,21 @@
 # combined utilitlies helpful to MCMC sampler
 # grab the functions from model_sim.py and data_simulation_1_radius.py
 # Require:
-#   - p_inte.cpp & p_inte.so
-#   - model_sim.py, ns_cov.py
-#   - RW_inte_mpmath.py
+#   - model_sim.py, p_inte.cpp & p_inte.so
+#   - RW_inte.py
+#   - ns_cov.py
 # %%
 # general imports and ubiquitous utilities
 import numpy as np
 from scipy.linalg import lapack
 from ns_cov import *
-import RW_inte_mpmath
+import RW_inte
 import model_sim
 import scipy
-import mpmath
 
 # specify integration and transformation
 #############################
-dRW_method = 'cpp'          #
-pRW_method = 'cpp'          #
-qRW_method = 'cpp'          #
+inte_method = 'cpp'         #
 # norm_pareto = 'shifted'   #
 norm_pareto = 'standard'    #
 #############################
@@ -52,46 +49,23 @@ else: # norm_pareto == 'standard'
 # %%
 # specify which dRW, pRW, and qRW to use
 
-dRW_cpp = model_sim.dRW
-pRW_cpp = model_sim.pRW
-def qRW_cpp(p, phi, gamma):
-    return(model_sim.qRW_Newton(p, phi, gamma, 100)) # qRW_Newton is vectorized
-
-dRW_mpmath = RW_inte_mpmath.dRW_mpmath_vec_float
-pRW_mpmath = RW_inte_mpmath.pRW_mpmath_vec_float
-qRW_mpmath = RW_inte_mpmath.qRW_mpmath_vec_float
-
-# using the GSL incomplete gamma function is much (10x) faster than mpmath
-upper_gamma_C = np.vectorize(model_sim.lib.upper_gamma_C, otypes = [float])
-lower_gamma_C = np.vectorize(model_sim.lib.lower_gamma_C, otypes = [float])
-# mpmath.mp.dps = 15
-def dRW_stdPareto(x, phi, gamma):
-    upper_gamma = upper_gamma_C(0.5 - phi, gamma / (2*np.power(x, 1/phi)))
-    # upper_gamma = float(mpmath.gammainc(0.5 - phi, a = gamma / (2*np.power(x, 1/phi))))
-    return (1/np.power(x,2)) * np.sqrt(1/np.pi) * np.power(gamma/2, phi) * upper_gamma
-def pRW_stdPareto(x, phi, gamma):
-    lower_gamma = lower_gamma_C(0.5, gamma / (2*np.power(x, 1/phi)))
-    upper_gamma = upper_gamma_C(0.5 - phi, gamma / (2*np.power(x, 1/phi)))
-    # lower_gamma = float(mpmath.gammainc(0.5, b = gamma / (2*np.power(x, 1/phi))))
-    # upper_gamma = float(mpmath.gammainc(0.5 - phi, a = gamma / (2*np.power(x, 1/phi))))
-    survival = np.sqrt(1/np.pi) * lower_gamma + (1/x) * np.sqrt(1/np.pi) * np.power(gamma/2, phi) * upper_gamma
-    return 1 - survival
-def qRW_stdPareto(p, phi, gamma):
-    return scipy.optimize.root_scalar(lambda x: pRW_stdPareto(x, phi, gamma) - p,
-                                      bracket=[0.1,1e12],
-                                      method='ridder').root
-dRW_stdPareto_vec = np.vectorize(dRW_stdPareto, otypes=[float])
-pRW_stdPareto_vec = np.vectorize(pRW_stdPareto, otypes=[float])
-qRW_stdPareto_vec = np.vectorize(qRW_stdPareto, otypes=[float])
-
 if norm_pareto == 'standard':
-    dRW = dRW_stdPareto_vec
-    pRW = pRW_stdPareto_vec
-    qRW = qRW_stdPareto_vec
+    dRW = RW_inte.dRW_stdPareto_vec
+    pRW = RW_inte.pRW_stdPareto_vec
+    qRW = RW_inte.qRW_stdPareto_vec
 else: # norm_pareto == 'shifted'
-    dRW = dRW_cpp if dRW_method == 'cpp' else dRW_mpmath
-    pRW = pRW_cpp if pRW_method == 'cpp' else pRW_mpmath
-    qRW = qRW_cpp if qRW_method == 'cpp' else qRW_mpmath
+    if inte_method == 'cpp':
+        dRW = RW_inte.dRW_cpp
+        pRW = RW_inte.pRW_cpp
+        qRW = RW_inte.qRW_cpp
+    elif inte_method == 'mpmath':
+        dRW = RW_inte.dRW_mpmath_vec_float
+        pRW = RW_inte.pRW_mpmath_vec_float
+        qRW = RW_inte.qRW_mpmath_vec_float
+    elif inte_method == 'scipy':
+        dRW = RW_inte.dRW_scipy
+        pRW = RW_inte.pRW_scipy
+        qRW = RW_inte.qRW_scipy
 
 # %%
 # marginal likelihood using integration and transformation specified above 
