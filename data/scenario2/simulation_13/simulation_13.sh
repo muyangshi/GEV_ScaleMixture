dba88e3a35da821f845e3a12cd1ae521273a4a6b
Muyang <34021089+muyangshi@users.noreply.github.com>
2023-11-06 17:38:33 -0700

    put data on misspiggy

---
+++ data/scenario2/simulation_13/simulation_13.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=csu70_alpine1
+#SBATCH --nodes 1
+#SBATCH --ntasks 64
+#SBATCH --job-name simulation_13
+#SBATCH --constraint ib
+#SBATCH --partition amilan
+#SBATCH --time 24:00:00
+#SBATCH --output simulation_13.out
+module purge
+module load gcc
+module load openmpi
+SLURM_EXPORT_ENV=ALL
+module load gsl
+module load boost
+module load anaconda
+conda activate testenv
+$CXX -std=c++11 -Wall -pedantic -I$CURC_GSL_INC -I$CURC_BOOST_INC -L$CURC_GSL_LIB -L$CURC_BOOST_LIB p_inte.cpp -shared -fPIC -o p_inte.so -lgsl -lgslcblas
+mpirun -n 64 python3 MCMC_fix_GEV.py 104
