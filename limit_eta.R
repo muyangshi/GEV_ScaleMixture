62ff1c56fc7cbe7633ad0aa3b0d10281451d06aa
Muyang <shimy17@icloud.com>
2024-10-14 10:07:56 -0600

    update with limit eta results on 100m and 300m

--- limit_eta.R
+++ limit_eta.R
@@ -12,7 +12,8 @@ library(VGAM)
 library(extRemes)
 
 # load the CDF_X_{Nt}
-load('eta_chi:CDF_X_1000000.gzip')
+load('eta_chi:CDF_X_300000000.gzip')
+print('loading CDF_X_300000000.gzip')
 S1 <- CDF_X[,1]
 S2 <- CDF_X[,2]
 S3 <- CDF_X[,3]
@@ -31,7 +32,7 @@ S6 <- CDF_X[,6]
 # S6_Frechet <- qevd(S6,type='Frechet', shape = 1)
 
 # can use the VGAM package's transformation
-S1_Frechet_VGAM <- qfrechet(S1, shape = 1)
+# S1_Frechet_VGAM <- qfrechet(S1, shape = 1)
 
 
 # using a manual transformation
@@ -51,42 +52,42 @@ S6_Frechet_my <- my_qFrechet(S6)
 # eta_12
 S12_my <- pmin(S1_Frechet_my, S2_Frechet_my)
 S12_fit_my <- fevd(S12_my, threshold=0, type="GP", method="GMLE")
-S12_fit_my$results$par
+print(S12_fit_my$results$par)
 #    scale    shape 
 # 1.158062 0.388302 
 
 # eta_34
 S34_my <- pmin(S3_Frechet_my, S4_Frechet_my)
 S34_fit_my <- fevd(S34_my, threshold=0, type="GP", method="GMLE")
-S34_fit_my$results$par
+print(S34_fit_my$results$par)
 #     scale     shape 
 # 1.1512180 0.2750359 
 
 # eta_45
 S45_my <- pmin(S4_Frechet_my, S5_Frechet_my)
 S45_fit_my <- fevd(S45_my, threshold=0, type="GP", method="GMLE")
-S45_fit_my$results$par
+print(S45_fit_my$results$par)
 #     scale     shape 
 # 1.1220280 0.2217238 
 
 # eta_15
 S15_my <- pmin(S1_Frechet_my, S5_Frechet_my)
 S15_fit_my <- fevd(S15_my, threshold=0, type="GP", method="GMLE")
-S15_fit_my$results$par
+print(S15_fit_my$results$par)
 #     scale     shape 
 # 1.1249631 0.1572785 
 
 # eta_36
 S36_my <- pmin(S3_Frechet_my, S6_Frechet_my)
 S36_fit_my <- fevd(S36_my, threshold=0, type="GP", method="GMLE")
-S36_fit_my$results$par
+print(S36_fit_my$results$par)
 #     scale     shape 
 # 1.1266590 0.1560476 
 
 # eta_14
 S14_my <- pmin(S1_Frechet_my, S4_Frechet_my)
 S14_fit_my <- fevd(S14_my, threshold=0, type="GP", method="GMLE")
-S14_fit_my$results$par
+print(S14_fit_my$results$par)
 #     scale     shape 
 # 1.1270437 0.1557162 
 
@@ -96,3 +97,31 @@ S14_fit_my$results$par
 
 
 
+# [1] "loading CDF_X_100000000.gzip"
+#     scale     shape 
+# 1.0103549 0.5482809 
+#    scale    shape 
+# 1.152733 0.274616 
+#     scale     shape 
+# 1.1243887 0.2201412 
+#     scale     shape 
+# 1.1272708 0.1568046 
+#    scale    shape 
+# 1.127313 0.156848 
+#     scale     shape 
+# 1.1272067 0.1566571 
+
+
+# [1] "loading CDF_X_300000000.gzip"
+#      scale      shape 
+#  1.1479780 -0.1208405 
+#     scale     shape 
+# 1.1528882 0.2747185 
+#     scale     shape 
+# 1.1242579 0.2203173 
+#     scale     shape 
+# 1.1273138 0.1566615 
+#     scale     shape 
+# 1.1272807 0.1568434 
+#     scale     shape 
+# 1.1271741 0.1569121 
